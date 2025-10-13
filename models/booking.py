a089f085b66d0b6111d4c19ec394bc230eb302e3
khayam <s4018445@student.rmit.edu.au>
2025-10-13 16:24:38 +1100

    test

--- models/booking.py
+++ models/booking.py
@@ -1,7 +1,7 @@
 import datetime
 
 class Booking:
-    def __init__(self, user_id, room_id, start_time, end_time, token, status='active', id=None):
+    def __init__(self, user_id, room_id, start_time, end_time, token, status='active', token_used=False, id=None):
         self.id = id
         self.user_id = user_id
         self.room_id = room_id
@@ -9,6 +9,7 @@ class Booking:
         self.end_time = end_time
         self.token = token
         self.status = status
+        self.token_used = token_used
 
     def __repr__(self):
         return f"<Booking(id={self.id}, user_id={self.user_id}, room_id={self.room_id}, status='{self.status}')>"
